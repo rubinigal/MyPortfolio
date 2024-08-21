f4fd16f0abaeb4f8a969f4dbdb89d2b975b141de
Igal <rubin.igal.w@gmail.com>
2024-08-21 15:20:23 +0300

    added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 //import { useState } from 'react'
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 //import './App.css'
 
 function App() {
   
-  const navList = ['Skills','Projects']
+  const navList = ['skills','projects']
 
   return (
     <>
      <Navbar tabs={navList} />
+     <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import '../styles/Footer.css'
+
+function Footer() {
+
+    return(
+        <p>© 2024 Igal Rubin. All rights reserved.</p>
+    );
+}
+
+export default Footer
