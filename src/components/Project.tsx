5ce6a4db5f543a5e3796c6c4ed7502e85828b0fb
Igal <rubin.igal.w@gmail.com>
2024-08-22 11:58:51 +0300

    added Project component, used by the Projects section

---
+++ src/components/Project.tsx
@@ -0,0 +1,20 @@
+type ProjectProps = {
+    title: string,
+    description: string,
+    image?: string,
+    isRightSide?: boolean
+}
+
+function Project({title, description, image="", isRightSide=true}: ProjectProps) {
+
+    const desc = <div className="project-desc"><h3>{title}</h3><p>{description}</p></div>;
+    const pic = <div className="project-pic"><img src={image} alt={title} /></div>;
+    
+    return(
+    <div className="project">
+       {isRightSide ? <>{desc} {pic}</> : <>{pic} {desc}</>}
+    </div>
+    );
+}
+
+export default Project
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,9 +1,29 @@
+import Project from "./Project";
 
 function ProjectsSection(sectionId: {id: string}) {
 
+    let isRightSide = false;
+    const projects = [
+        { 
+            title: "Website: Rikmot Al",
+            description: "A website for a company named Rikmot Al",
+            skills: "HTML, CSS, JavaScript, TypeScript, React.js, Vite and npm",
+            image: ""
+        },
+        {
+            title: "Custom Language Compiler",
+            description: "As part of my coursework at my university, I had the invaluable opportunity to embark on a challenging and enriching project to develop a compiler for a custom programming language called. The goal of this project was to create a fully functional compiler capable of translating human-readable source code written in the custom language into executable machine code.",
+            skills: "C, VMWARE, Ubuntu run by Linux/Unix",
+            image: ""
+        }
+    ]
+
     return(
     <section id={sectionId.id} className="projects-section">
-        
+        {projects.map(project => {
+            isRightSide = !isRightSide
+            return <Project title={project.title} description={project.description} image={project.image} isRightSide={isRightSide} />
+            })}
     </section>
     );
 }
